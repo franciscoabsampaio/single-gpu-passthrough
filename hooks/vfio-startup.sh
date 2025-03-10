d5717154e5092aa3cae45747610719775414f391
franciscoabsampaio <franciscoabsampaio@proton.me>
2025-03-10 15:39:19 +0000

    Fix: hardcode vtconsole number. Disable AMD GPU unloading.

--- hooks/vfio-startup.sh
+++ hooks/vfio-startup.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-#############################################################################
-##     ______  _                _  _______         _                 _     ##
-##    (_____ \(_)              | |(_______)       | |               | |    ##
-##     _____) )_  _   _  _____ | | _    _   _   _ | |__   _____   __| |    ##
-##    |  ____/| |( \ / )| ___ || || |  | | | | | ||  _ \ | ___ | / _  |    ##
-##    | |     | | ) X ( | ____|| || |__| | | |_| || |_) )| ____|( (_| |    ##
-##    |_|     |_|(_/ \_)|_____) \_)\______)|____/ |____/ |_____) \____|    ##
-##                                                                         ##
-#############################################################################
-###################### Credits ###################### ### Update PCI ID'S ###
-## Lily (PixelQubed) for editing the scripts       ## ##                   ##
-## RisingPrisum for providing the original scripts ## ##   update-pciids   ##
-## Void for testing and helping out in general     ## ##                   ##
-## .Chris. for testing and helping out in general  ## ## Run this command  ##
-## WORMS for helping out with testing              ## ## if you dont have  ##
-##################################################### ## names in you're   ##
-## The VFIO community for using the scripts and    ## ## lspci feedback    ##
-## testing them for us!                            ## ## in your terminal  ##
-##################################################### #######################
-
 ################################# Variables #################################
 
 ## Adds current time to var for use in echo for a cleaner log and script ##
@@ -30,14 +10,14 @@ DISPMGR="null"
 
 ################################## Script ###################################
 
-echo "$DATE Beginning of Startup!"
+logger "$DATE Beginning of Startup!"
 
 
 function stop_display_manager_if_running {
     ## Get display manager on systemd based distros ##
-    if [[ -x /run/systemd/system ]] && echo "$DATE Distro is using Systemd"; then
+    if [[ -x /run/systemd/system ]] && logger "$DATE Distro is using Systemd"; then
         DISPMGR="$(grep 'ExecStart=' /etc/systemd/system/display-manager.service | awk -F'/' '{print $(NF-0)}')"
-        echo "$DATE Display Manager = $DISPMGR"
+        logger "$DATE Display Manager = $DISPMGR"
 
         ## Stop display manager using systemd ##
         if systemctl is-active --quiet "$DISPMGR.service"; then
@@ -58,7 +38,7 @@ function stop_display_manager_if_running {
 
 function kde-clause {
 
-    echo "$DATE Display Manager = display-manager"
+    logger "$DATE Display Manager = display-manager"
 
     ## Stop display manager using systemd ##
     if systemctl is-active --quiet "display-manager.service"; then
@@ -68,7 +48,7 @@ function kde-clause {
     fi
 
         while systemctl is-active --quiet "display-manager.service"; do
-                sleep 2
+            sleep 2
         done
 
     return
@@ -81,10 +61,10 @@ function kde-clause {
 ####################################################################################################################
 
 if pgrep -l "plasma" | grep "plasmashell"; then
-    echo "$DATE Display Manager is KDE, running KDE clause!"
+    logger "$DATE Display Manager is KDE, running KDE clause!"
     kde-clause
     else
-        echo "$DATE Display Manager is not KDE!"
+        logger "$DATE Display Manager is not KDE!"
         stop_display_manager_if_running
 fi
 
@@ -101,24 +81,27 @@ sleep "1"
 ##############################################################################################################################
 ## Unbind VTconsoles if currently bound (adapted and modernised from https://www.kernel.org/doc/Documentation/fb/fbcon.txt) ##
 ##############################################################################################################################
-if test -e "/tmp/vfio-bound-consoles"; then
-    rm -f /tmp/vfio-bound-consoles
-fi
-for (( i = 0; i < 16; i++))
-do
-  if test -x /sys/class/vtconsole/vtcon"${i}"; then
-      if [ "$(grep -c "frame buffer" /sys/class/vtconsole/vtcon"${i}"/name)" = 1 ]; then
-	       echo 0 > /sys/class/vtconsole/vtcon"${i}"/bind
-           echo "$DATE Unbinding Console ${i}"
-           echo "$i" >> /tmp/vfio-bound-consoles
-      fi
-  fi
-done
+logger "$DATE Unbinding Console 0"
+echo 0 > /sys/class/vtconsole/vtcon0/bind
+
+# if test -e "/tmp/vfio-bound-consoles"; then
+#     rm -f /tmp/vfio-bound-consoles
+# fi
+# for (( i = 0; i < 16; i++))
+# do
+#   if test -x /sys/class/vtconsole/vtcon"${i}"; then
+#       if [ "$(grep -c "frame buffer" /sys/class/vtconsole/vtcon"${i}"/name)" = 1 ]; then
+# 	       echo 0 > /sys/class/vtconsole/vtcon"${i}"/bind
+#            logger "$DATE Unbinding Console ${i}"
+#            echo "$i" >> /tmp/vfio-bound-consoles
+#       fi
+#   fi
+# done
 
 sleep "1"
 
 if lspci -nn | grep -e VGA | grep -s NVIDIA ; then
-    echo "$DATE System has an NVIDIA GPU"
+    logger "$DATE System has an NVIDIA GPU"
     grep -qsF "true" "/tmp/vfio-is-nvidia" || echo "true" >/tmp/vfio-is-nvidia
     echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
 
@@ -131,26 +114,26 @@ if lspci -nn | grep -e VGA | grep -s NVIDIA ; then
     modprobe -r drm_kms_helper
     modprobe -r drm
 
-    echo "$DATE NVIDIA GPU Drivers Unloaded"
+    logger "$DATE NVIDIA GPU Drivers Unloaded"
 fi
 
-if lspci -nn | grep -e VGA | grep -s AMD ; then
-    echo "$DATE System has an AMD GPU"
-    grep -qsF "true" "/tmp/vfio-is-amd" || echo "true" >/tmp/vfio-is-amd
-    echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
+# if lspci -nn | grep -e VGA | grep -s AMD ; then
+#     logger "$DATE System has an AMD GPU"
+#     grep -qsF "true" "/tmp/vfio-is-amd" || echo "true" >/tmp/vfio-is-amd
+#     echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
 
-    ## Unload AMD GPU drivers ##
-    modprobe -r drm_kms_helper
-    modprobe -r amdgpu
-    modprobe -r radeon
-    modprobe -r drm
+#     ## Unload AMD GPU drivers ##
+#     modprobe -r drm_kms_helper
+#     modprobe -r amdgpu
+#     modprobe -r radeon
+#     modprobe -r drm
 
-    echo "$DATE AMD GPU Drivers Unloaded"
-fi
+#     logger "$DATE AMD GPU Drivers Unloaded"
+# fi
 
 ## Load VFIO-PCI driver ##
 modprobe vfio
 modprobe vfio_pci
 modprobe vfio_iommu_type1
 
-echo "$DATE End of Startup!"
+logger "$DATE End of Startup!"
--- hooks/vfio-teardown.sh
+++ hooks/vfio-teardown.sh
@@ -1,44 +1,20 @@
 #!/bin/bash
 
-#############################################################################
-##     ______  _                _  _______         _                 _     ##
-##    (_____ \(_)              | |(_______)       | |               | |    ##
-##     _____) )_  _   _  _____ | | _    _   _   _ | |__   _____   __| |    ##
-##    |  ____/| |( \ / )| ___ || || |  | | | | | ||  _ \ | ___ | / _  |    ##
-##    | |     | | ) X ( | ____|| || |__| | | |_| || |_) )| ____|( (_| |    ##
-##    |_|     |_|(_/ \_)|_____) \_)\______)|____/ |____/ |_____) \____|    ##
-##                                                                         ##
-#############################################################################
-###################### Credits ###################### ### Update PCI ID'S ###
-## Lily (PixelQubed) for editing the scripts       ## ##                   ##
-## RisingPrisum for providing the original scripts ## ##   update-pciids   ##
-## Void for testing and helping out in general     ## ##                   ##
-## .Chris. for testing and helping out in general  ## ## Run this command  ##
-## WORMS for helping out with testing              ## ## if you dont have  ##
-##################################################### ## names in you're   ##
-## The VFIO community for using the scripts and    ## ## lspci feedback    ##
-## testing them for us!                            ## ## in your terminal  ##
-##################################################### #######################
-
-################################# Variables #################################
-
-## Adds current time to var for use in echo for a cleaner log and script ##
+# Define the current date/time for logging
 DATE=$(date +"%m/%d/%Y %R:%S :")
 
-################################## Script ###################################
+# Log the start of the script
+logger "$DATE Beginning of Teardown!"
 
-echo "$DATE Beginning of Teardown!"
-
-## Unload VFIO-PCI driver ##
+# Log VFIO unloads
+logger "$DATE Unloading VFIO drivers..."
 modprobe -r vfio_pci
 modprobe -r vfio_iommu_type1
 modprobe -r vfio
 
-if grep -q "true" "/tmp/vfio-is-nvidia" ; then
-
-    ## Load NVIDIA drivers ##
-    echo "$DATE Loading NVIDIA GPU Drivers"
-    
+if grep -q "true" "/tmp/vfio-is-nvidia"; then
+    # Log if we are using NVIDIA drivers
+    logger "$DATE Loading NVIDIA GPU Drivers"
     modprobe drm
     modprobe drm_kms_helper
     modprobe i2c_nvidia_gpu
@@ -46,54 +22,47 @@ if grep -q "true" "/tmp/vfio-is-nvidia" ; then
     modprobe nvidia_modeset
     modprobe nvidia_drm
     modprobe nvidia_uvm
-
-    echo "$DATE NVIDIA GPU Drivers Loaded"
+    logger "$DATE NVIDIA GPU Drivers Loaded"
 fi
 
-if  grep -q "true" "/tmp/vfio-is-amd" ; then
-
-    ## Load AMD drivers ##
-    echo "$DATE Loading AMD GPU Drivers"
-    
+if grep -q "true" "/tmp/vfio-is-amd"; then
+    # Log if we are using AMD drivers
+    logger "$DATE Loading AMD GPU Drivers"
     modprobe drm
     modprobe amdgpu
     modprobe radeon
     modprobe drm_kms_helper
-    
-    echo "$DATE AMD GPU Drivers Loaded"
+    logger "$DATE AMD GPU Drivers Loaded"
 fi
 
-## Restart Display Manager ##
+# Restart Display Manager and log each step
 input="/tmp/vfio-store-display-manager"
 while read -r DISPMGR; do
-  if command -v systemctl; then
-
-    ## Make sure the variable got collected ##
-    echo "$DATE Var has been collected from file: $DISPMGR"
-
-    systemctl start "$DISPMGR.service"
-
-  else
-    if command -v sv; then
-      sv start "$DISPMGR"
+    logger "$DATE Trying to start display manager: $DISPMGR"
+    if command -v systemctl; then
+        logger "$DATE Starting $DISPMGR service"
+        systemctl start "$DISPMGR.service"
+    else
+        if command -v sv; then
+            logger "$DATE Starting $DISPMGR with sv"
+            sv start "$DISPMGR"
+        fi
     fi
-  fi
 done < "$input"
 
-############################################################################################################
-## Rebind VT consoles (adapted and modernised from https://www.kernel.org/doc/Documentation/fb/fbcon.txt) ##
-############################################################################################################
+# Rebind VT consoles and log each console that is rebinding
+consoleNumber=0
+logger "$DATE Rebinding console $consoleNumber"
+echo 1 > /sys/class/vtconsole/vtcon"${consoleNumber}"/bind
+# input="/tmp/vfio-bound-consoles"
+# while read -r consoleNumber; do
+#     if test -x /sys/class/vtconsole/vtcon"${consoleNumber}"; then
+#         if [ "$(grep -c "frame buffer" "/sys/class/vtconsole/vtcon${consoleNumber}/name")" = 1 ]; then
+#             logger "$DATE Rebinding console $consoleNumber"
+#             echo 1 > /sys/class/vtconsole/vtcon"${consoleNumber}"/bind
+#         fi
+#     fi
+# done < "$input"
 
-input="/tmp/vfio-bound-consoles"
-while read -r consoleNumber; do
-  if test -x /sys/class/vtconsole/vtcon"${consoleNumber}"; then
-      if [ "$(grep -c "frame buffer" "/sys/class/vtconsole/vtcon${consoleNumber}/name")" \
-           = 1 ]; then
-    echo "$DATE Rebinding console ${consoleNumber}"
-	  echo 1 > /sys/class/vtconsole/vtcon"${consoleNumber}"/bind
-      fi
-  fi
-done < "$input"
-
-
-echo "$DATE End of Teardown!"
+# Final log entry
+logger "$DATE End of Teardown!"
